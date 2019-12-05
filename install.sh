0bf040ab4cd48eebff98fe61639632af116d8325
Josh Currier <doitaljosh@gmail.com>
2019-12-04 20:34:38 -0500

    Fixed install script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 CONFIG_PATH='/etc/mxcam'
 FW_PATH='/lib/firmware/gc6500'
+UDEV_FILE='/etc/udev/rules.d/500-gc6500.rules'
 
 mkdir -p $CONFIG_PATH
 mkdir -p $FW_PATH
@@ -13,7 +14,7 @@ cp config/* $CONFIG_PATH/
 chmod +x /usr/bin/mxcam
 chmod +x /usr/bin/boot_camera.sh
 
-touch /etc/udev/rules.d/500-gc6500.rules
-echo -n 'ATTR{idVendor} =="29fe", ATTR{idProduct} =="b00c", MODE="0660", OWNER="root", GROUP="video", RUN+="/usr/bin/boot_camera.sh"' > /etc/udev/rules.d/500-gc6500.rules
+echo -n 'ATTR{idVendor} =="29fe", ATTR{idProduct} =="b00c", MODE="0660", OWNER="root", GROUP="video", RUN+="/usr/bin/boot_camera.sh"' \
+> $UDEV_FILE
 
 echo "Installation complete."
