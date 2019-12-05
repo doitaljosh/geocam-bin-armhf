8ddb489ef88922d8c08749116d66feba051b1d38
Josh Currier <doitaljosh@gmail.com>
2019-12-04 20:12:18 -0500

    Revised readme

--- install.sh
+++ install.sh
@@ -7,10 +7,11 @@ mkdir -p $CONFIG_PATH
 mkdir -p $FW_PATH
 
 cp gc6500_ddrboot_fw.img sensor_ov2710_mayfield_le.bin $FW_PATH/
-cp mxcam /usr/bin
+cp mxcam boot_camera.sh /usr/bin
 cp config.json $CONFIG_PATH/
 
 chmod +x /usr/bin/mxcam
+chmod +x /usr/bin/boot_camera.sh
 
 touch /etc/udev/rules.d/500-gc6500.rules
 echo -n 'ATTR{idVendor} =="29fe", ATTR{idProduct} =="b00c", MODE="0660", OWNER="root", GROUP="video", RUN+="/usr/bin/boot_camera.sh"' > /etc/udev/rules.d/500-gc6500.rules
